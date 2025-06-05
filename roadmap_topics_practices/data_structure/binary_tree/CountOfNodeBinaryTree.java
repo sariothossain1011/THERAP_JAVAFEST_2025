37c63245759170a7bceaf2af0cfd3eee6eddc5fb
Sariot Hossain <sariothossain1011@gmail.com>
2025-06-05 17:52:29 +0600

    practicing dsa binary tree

---
+++ roadmap_topics_practices/data_structure/binary_tree/CountOfNodeBinaryTree.java
@@ -0,0 +1,49 @@
+package roadmap_topics_practices.data_structure.binary_tree;
+
+public class CountOfNodeBinaryTree {
+        static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int index = -1;
+
+        public static Node buildTree(int nodes[]) {
+            index++;
+            if (nodes[index] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[index]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+
+        }
+    }
+    public static int countOfNodes(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftNodes = countOfNodes(root.left);
+        int rightNodes = countOfNodes(root.right);
+        return leftNodes + rightNodes + 1;
+
+    }
+
+    public static void main(String[] args) {
+                int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(countOfNodes(root));
+
+    }
+
+}
